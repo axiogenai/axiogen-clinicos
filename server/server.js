1ec474599ec1c0d5286161520208f9edb61d16b9
AXIOGEN <adityaminchekarfx@gmail.com>
2026-08-07 16:57:30 +0530

    Add trust proxy setting for clean rate limiter logging: server/server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const app = express();
+app.set('trust proxy', 1);
+
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
