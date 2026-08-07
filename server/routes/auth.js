25ab7ad2c2c68afbfb5fc2e240554be418802010
AXIOGEN <adityaminchekarfx@gmail.com>
2026-08-07 21:05:27 +0530

    Add Supabase Auth, input validations & forgot password OTP reset: server/routes/auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,5 +6,8 @@ const { verifyToken } = require('../middleware/auth');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/me', verifyToken, authController.getMe);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/verify-otp', authController.verifyOTP);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
